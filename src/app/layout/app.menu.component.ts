da6eebed03073ec106f6c0d0aaf968c54aee0616
Sergio Nicolas Pinilla <spinillac@car.gov.co>
2023-10-07 09:23:43 -0500

    Cambios layout

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -50,7 +50,7 @@ export class AppMenuComponent implements OnInit {
 
                 ]
             },
-
+            /*
             {
                 label: 'UI Components',
                 items: [
@@ -191,7 +191,7 @@ export class AppMenuComponent implements OnInit {
                         label: 'View Source', icon: 'pi pi-fw pi-search', url: ['https://github.com/primefaces/sakai-ng'], target: '_blank'
                     }
                 ]
-            }
+            }*/
         ];
     }
 }
